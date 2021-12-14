efa69f0778ec14a176ee4f5a2d873c1381408e54
VladRaileanu <raileanu.vlad@yahoo.com>
2021-12-15 01:41:01 +0200

    refactored the random characters generated password function, refactored the dictionary words generated password and also finished it ( a lot of use cases ) and also managed to use the sys library in order to be able to run the script from the command line with arguments so the script knows which fuction to call.

--- generate_password.py
+++ generate_password.py
@@ -1,52 +1,145 @@
 import random
 import string
+import sys
 
 
 def print_random_password():
     length = random.randint(12, 18)
     characters = string.ascii_letters + string.digits + random.choice('@!#?')
     first_uppercase = random.choice(string.ascii_uppercase)
-    random_symbol_pos = random.randint(0, 15)
-    rest_of_the_word = ''
 
-    for i in range(length - 2):
-        rest_of_the_word = rest_of_the_word + random.choice(characters)
+    required = [random.choice(string.digits), random.choice('@!#?')]
+    while(len(required) != length - 1):
+        required.append(random.choice(characters))
 
-    final_rest_of_the_word = rest_of_the_word[:random_symbol_pos] + random.choice('@!#?') + rest_of_the_word[
-                                                                                            random_symbol_pos:]
-    final_word = first_uppercase + final_rest_of_the_word
-
+    random.shuffle(required)
+    final_word = ''.join([str(e) for e in required])
+    final_word = first_uppercase + final_word
     print(final_word)
+    # print(f"Number of ch: {len(final_word)}")
 
+def random_digit_or_special_ch():
+    ch = ''
+    if (random.randint(0, 1)):
+        ch += random.choice('@!#?')
+    else:
+        ch += random.choice(string.digits)
+    return ch
 
-def print_dictionary_password():
-    length = random.randint(12, 18)
-    password_length = 0
+def random_digit_and_special_ch():
+    ch = ''
+    if (random.randint(0, 1)):
+        ch += random.choice('@!#?')
+        ch += random.choice(string.digits)
+    else:
+        ch += random.choice(string.digits)
+        ch += random.choice('@!#?')
+    return ch
+
+def print_dictionary_password(dictionary_file):
     assembled_password = ''
-    is_first_word_valid = False
-    while not is_first_word_valid:
-        random_first_word = random.choice(open("dictionar.txt", "r").readline().split())
-        if random_first_word[0].isupper():
-            is_first_word_valid = True
+    first_words = []
+    all_words = []
+    for i in (open(f"{dictionary_file}", "r").readline().split()):
+        if(len(i) > 16):
+            continue
+        if i[0].isupper():
+            first_words.append(i)
 
-    while password_length <= length - len(random_first_word):
+        all_words.append(i)
 
-        random_word = random.choice(open("dictionar.txt", "r").readline().split())
-        assembled_password = assembled_password + random.choice(open("dictionar.txt", "r").readline().split()) + ' '
-        password_length = len(assembled_password.replace(' ', ''))
-        print('length', length)
-        print('firstword', len(random_first_word))
-        print(password_length)
-        if password_length > length - len(random_first_word):
-            for i in range(length - len(random_first_word)):
-                if assembled_password[i] == ' ':
-                    assembled_password_symbol = assembled_password[:i] + random.choice('@!#?') + assembled_password[i:]
-            final_assembled_password = (assembled_password_symbol.removesuffix(random_word)).replace(' ', '')
+    if(len(first_words) == 0):
+        print('Invalid dictionary: Missing uppercase word or the uppercase words are too long')
+        return
 
-    final_final_password = random_first_word + final_assembled_password
-    print(final_final_password)
+
+    copy_of_first_words = first_words
+    while(1):
+        assembled_password += random.choice(copy_of_first_words)
+
+        if(len(assembled_password) == 16):
+            assembled_password += random_digit_and_special_ch()
+            break
+
+        other_words = ''
+        copy_of_words = all_words
+
+        # Choosing next random words
+        while(1):
+            if(len(copy_of_words) == 0):
+                break
+
+            if(random.randint(0, 1)):
+                break
+
+            random_word = random.choice(copy_of_words)
+            if(len(random_word) > 16 - len(assembled_password) - len(other_words)):
+                copy_of_words.pop(copy_of_words.index(random_word))
+            else:
+                other_words += random_word
+
+
+        # If there were no other words picked or they got skipped
+        if(len(other_words) == 0):
+
+            # Random for deciding to continue the search for a new pair of upper word with other words
+            if (random.randint(0, 1)):
+                assembled_password += random_digit_and_special_ch()
+
+                # Checks the required minimal length
+                length = len(assembled_password)
+                if(length < 12):
+                    length = random.randint(12, 18)
+                else:
+                    print(f"Length: {length}  Pass: {assembled_password}")
+                    length = random.randint(len(assembled_password), 18)
+
+                # Fills the rest of the password with numbers and special characters up to the random length
+                while(len(assembled_password) != length):
+                    assembled_password += random_digit_or_special_ch()
+
+                break
+
+            else:
+                # If there are more then one uppercase words in the list
+                if(len(copy_of_first_words) != 1):
+                    # Removes the current uppercase word from the list
+                    copy_of_first_words.pop(copy_of_first_words.index(assembled_password))
+
+                assembled_password = ''
+
+        # Combines the uppercase word with the other words picked and random digits and special characters
+        else:
+
+            # Random for deciding the order for the digits and special characters and the other words
+            required = [other_words, random.choice('@!#?'), random.choice(string.digits)]
+            random.shuffle(required)
+            for i in required:
+                assembled_password += i
+
+            # Checks the required minimal length
+            length = len(assembled_password)
+            if (length < 12):
+                length = random.randint(12, 18)
+            else:
+                length = random.randint(len(assembled_password), 18)
+
+            # Fills the rest of the password with numbers and special characters up to the random length
+            while (len(assembled_password) != length):
+                assembled_password += random_digit_or_special_ch()
+            break
+
+
+    print(assembled_password)
+    # print(f"Number of ch: {len(assembled_password)}")
 
 
 if __name__ == '__main__':
-    print_random_password()
-    print_dictionary_password()
+
+    if len(sys.argv) == 1:
+        print_random_password()
+    elif sys.argv[1] == "-use_dict":
+        dictionary_file = sys.argv[2]
+        print_dictionary_password(dictionary_file)
+    else:
+        print("Unknown command")
